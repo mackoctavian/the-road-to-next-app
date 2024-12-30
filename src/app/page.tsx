271474b365c0806847de8141a9c56fcc6fc8cfd0
Mack Octavian <maestrooctavian@gmail.com>
2024-12-30 18:38:28 +0300

    Added conditional classes using clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import {ticketsPath} from '@/path';
 
 export default function HomePage() {
   return (
-    <main>
-      <h2>Home Page</h2>
-      <Link href={ticketsPath()} className="underline">Go to tickets</Link>
+    <main className="flex flex-1 flex-col items-center gap-y-8">
+      <div>
+      <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+      <p className="text-sm text-muted-foreground">Your home place to start</p>
+      </div>
+     <div className="flex-1 flex flex-col items-center">
+     <Link href={ticketsPath()} className="underline">Go to tickets</Link>
+     </div>
     </main>
   );
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,27 +1,38 @@
-
-import {initialTickets} from '../../data';
-import {ticketPath} from "@/path";
-import Link from 'next/link';
+import { initialTickets } from "../../data";
+import { ticketPath } from "@/path";
+import Link from "next/link";
+import clsx from "clsx";
 
 const TICKET_ICONS = {
-OPEN: 'O',
-DONE: 'X',
-IN_PROGRESS: '>',
-}
+  OPEN: "O",
+  DONE: "X",
+  IN_PROGRESS: ">",
+};
 
 export default function TicketsPage() {
   return (
-    <main>
-      {
-        initialTickets.map(ticket => (
-          <div key={ticket.id}>
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h2 className="text-lg">{ticket.title}</h2>
-            <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
+    <main className="flex flex-1 flex-col items-center gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets in one place
+        </p>
+      </div>
 
-          </div>
-        ))
-      }
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
+          {/* <div>{TICKET_ICONS[ticket.status]}</div> */}
+          <h3 className="text-lg truncate font-semibold">{ticket.title}</h3>
+          <p className={clsx("text-sm text-slate-500 truncate", {
+            "line-through": ticket.status === "DONE",
+          })}>{ticket.content}</p>
+          <Link href={ticketPath(ticket.id)} className="text-sm underline">
+            View
+          </Link>
+        </div>
+      ))}
+      </div>
     </main>
   );
 }
