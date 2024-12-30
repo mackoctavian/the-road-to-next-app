d85625601bdb9d307a2fc4403f5c3009087b9897
Mack Octavian <maestrooctavian@gmail.com>
2024-12-30 17:46:03 +0300

    const assertion to fix typescript errors

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,12 +2,20 @@
 import {initialTickets} from '../../data';
 import {ticketPath} from "@/path";
 import Link from 'next/link';
+
+const TICKET_ICONS = {
+OPEN: 'O',
+DONE: 'X',
+IN_PROGRESS: '>',
+}
+
 export default function TicketsPage() {
   return (
     <main>
       {
         initialTickets.map(ticket => (
           <div key={ticket.id}>
+            <div>{TICKET_ICONS[ticket.status]}</div>
             <h2 className="text-lg">{ticket.title}</h2>
             <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
 
--- src/data.ts
+++ src/data.ts
@@ -2,15 +2,15 @@ export const initialTickets = [{
   id: 1,
   title: 'Ticket 1',
   content: 'This is the first ticket',
-  status: 'DONE'
+  status: 'DONE' as const
 }, {
   id: 2,
   title: 'Ticket 2',
   content: 'This is the second ticket',
-  status: 'DONE'
+  status: 'DONE' as const
 }, {
   id: 3,
   title: 'Ticket 3',
   content: 'This is the third ticket',
-  status: 'OPEN'
+  status: 'IN_PROGRESS' as const
 }]
