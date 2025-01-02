3ec56290c2a0644037c3ab873b3e2a20c8d10e81
Mack Octavian <maestrooctavian@gmail.com>
2025-01-02 15:40:24 +0300

    Added error boundary to tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,15 +6,19 @@ import TicketItem from "@/features/ticket/components/ticket-item";
 import TicketList from "@/features/ticket/components/ticket-list";
 import { getTickets } from "@/features/ticket/queries/get-tickets";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import Placeholder from "@/components/placeholder";
 
 export default function TicketsPage() {
   return (
     <main className="flex flex-1 flex-col items-center gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
 
-      <Suspense fallback={<Spinner />}>
-        <TicketList />
-      </Suspense>
+      <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -3,6 +3,8 @@ import { Ticket } from "../type"
 
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  // throw new Error("Failed to fetch tickets")
   return new Promise((resolve) => {
 
       resolve(initialTickets)
