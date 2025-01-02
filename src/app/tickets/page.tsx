9c43c342fcf3a4cb1ad80536296f98ad5565bde9
Mack Octavian <maestrooctavian@gmail.com>
2025-01-02 13:54:19 +0300

    Added a server side data fetching logic

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,15 +1,21 @@
+"use client";
 import { initialTickets } from "../../data";
 
 import Heading from "@/components/heading";
 import TicketItem from "@/features/ticket/components/ticket-item";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { Ticket } from "@/features/ticket/type";
+import { useEffect, useState } from "react";
 
-export default function TicketsPage() {
+export default async function TicketsPage() {
+  const tickets = await getTickets();
+
   return (
     <main className="flex flex-1 flex-col items-center gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
